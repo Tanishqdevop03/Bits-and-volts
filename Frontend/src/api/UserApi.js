4d3f8a55b4b4d7141363d87c992fe462da87bc44
Tanishq Garg <tanishqtanishq56@gmail.com>
2026-02-19 07:47:15 +0530

    new cors

--- Frontend/src/api/UserApi.js
+++ Frontend/src/api/UserApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/users",
+  baseURL: "https://sweet-amazement-production.up.railway.app//api/users",
 });
 
 export const getUsers = (params) => API.get("/", { params });
@@ -10,4 +10,4 @@ export const createUser = (data) => API.post("/", data);
 export const updateUser = (id, data) => API.put(`/${id}`, data);
 export const deleteUser = (id) => API.delete(`/${id}`);
 export const exportCSV = () =>
-  window.open("http://localhost:5000/api/users/export/csv");
+  window.open("https://sweet-amazement-production.up.railway.app/api/users/export/csv");
